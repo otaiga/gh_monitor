f4ddb5d5fac88bd26e5564ad1016f38c6427f81e
Karl Turner <karl.turner5@gmail.com>
2014-05-22 22:51:08 +0100

    updated to include sound

--- support/arduino_client.rb
+++ support/arduino_client.rb
@@ -3,8 +3,9 @@ require 'dino'
 
 # Simple class for arduino connection
 class ArduinoClient
-  def blink(duration = 10, sleep_count = 1)
+  def blink(duration = 5, sleep_count = 1)
     states.cycle(duration) do |switch|
+      create_sound
       led_red.send(switch)
       sleep(sleep_count.to_i)
     end
@@ -17,8 +18,21 @@ class ArduinoClient
     ]
   end
 
+  def create_sound
+    [0.2, 0.1, 0.03, 0.5].each do |i|
+      sound.send(:on)
+      sleep(i)
+      sound.send(:off)
+      sleep(i)
+    end
+  end
+
   def led_red
     Dino::Components::Led.new(pin: 13, board: board)
+  end
+
+  def sound
+    Dino::Components::Led.new(pin: 8, board: board)
   end
 
   def board
